b2f708873a16aa2ea6548c244b31bd7e199e99e6
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-08-22 23:38:51 +0200

    Add write gui action controller scaffold

---
+++ Controllers/WriteGuiActionController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Digdir.BDB.Dialogporten.ServiceProvider.Controllers;
+
+[ApiController]
+[Authorize(AuthenticationSchemes = "DialogToken")]
+[Route("guiaction/write/{xacmlaction?}")]
+[EnableCors("AllowedOriginsPolicy")]
+public class WriteGuiActionController : Controller
+{
+    [HttpPost]
+    public IActionResult Post(string? xacmlaction, [FromQuery] bool isDelayed = false)
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Put(string? xacmlaction, [FromQuery] bool isDelayed = false)
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(string? xacmlaction, [FromQuery] bool isDelayed = false)
+    {
+        return Ok();
+    }
+}
